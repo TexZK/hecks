36ed19063ee01113ff163f69309e939912c27f43
Andrea Zoppi <texzk@email.it>
2022-11-12 01:07:07 +0100

    Updated project meta files as per `bytesparse`

--- docs/reference.rst
+++ docs/reference.rst
@@ -3,3 +3,13 @@ Reference
 
 .. autosummary::
     :toctree: _autosummary
+    :template: custom-module-template.rst
+    :recursive:
+
+    hecks
+    hecks.cli
+    hecks.common
+    hecks.engine
+    hecks.memento
+    hecks.tkgui
+    hecks.utils
--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+
 import io
+import os
 import re
 from glob import glob
-from os.path import basename
-from os.path import dirname
-from os.path import join
-from os.path import splitext
 
 from setuptools import find_packages
 from setuptools import setup
@@ -14,18 +12,28 @@ from setuptools import setup
 
 def read(*names, **kwargs):
     return io.open(
-        join(dirname(__file__), *names),
+        os.path.join(os.path.dirname(__file__), *names),
         encoding=kwargs.get('encoding', 'utf8')
     ).read()
 
 
+def read_version():
+    path = os.path.join('src', 'bytesparse', '__init__.py')
+    with open(path, 'rt') as file:
+        for line in file:
+            if line.startswith('__version__'):
+                return eval(line.split('=')[1])
+    raise ValueError(f'cannot find __version__ inside of {path}')
+
+
 setup(
     name='hecks',
-    version='0.0.1a0',
+    version=read_version(),
     license='GNU General Public License version 3 License',
     description='Hexadecimal editor',
     long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S)
+            .sub('', read('README.rst')),
         re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
     ),
     long_description_content_type='text/x-rst',
@@ -34,11 +42,13 @@ setup(
     url='https://github.com/TexZK/hecks',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob('src/*.py')],
+    include_dirs=['.'],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        # complete classifier list at: https://pypi.org/classifiers/
+        # complete classifier list:
+        # http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
@@ -48,6 +58,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Scientific/Engineering',
@@ -68,5 +79,8 @@ setup(
         # 'console_scripts': [
         #     'hecks = hecks.cli:main',
         # ],
+        'testing': [
+            'pytest',
+        ],
     },
 )
